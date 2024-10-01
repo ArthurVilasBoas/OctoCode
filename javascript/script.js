adac443c5719a2219418319deb804138c1a9ed78
ArthurVilasBoas <arthurvilasboas011@gmail.com>
2024-10-01 01:47:23 -0300

    consegui fazer a setinha

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,17 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval( function () {
-  nextImage();  
+setInterval(function () {
+  nextImage();
 }, 5000)
 
 
 function nextImage() {
-    count++;
-    if (count>3) {
-        count = 1;
-    }
-    document.getElementById("radio"+count).checked = true;
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
 }
 
 
@@ -20,19 +20,28 @@ function nextImage() {
 const overlay = document.querySelector('.overlay');
 const navbar = document.getElementById('SideBar');
 
-navbar.addEventListener("mouseover", ()=>{
+navbar.addEventListener("mouseover", () => {
   acionaOverlay();
 });
-navbar.addEventListener("mouseout", ()=>{
+navbar.addEventListener("mouseout", () => {
   removeOverlay();
 });
 
-function acionaOverlay(){
+function acionaOverlay() {
   overlay.style.display = 'block';
   overlay.style.filter = 'blur(1.7rem)';
   console.log("Zika de mais ");
 }
 
-function removeOverlay(){
+function removeOverlay() {
   overlay.style.display = 'none';
 }
+
+function clickMenu() {
+  if (SideBar.style.display == 'block') {
+    SideBar.style.display = 'none'
+  } else {
+    SideBar.style.display = 'block'
+        
+    }
+  }
